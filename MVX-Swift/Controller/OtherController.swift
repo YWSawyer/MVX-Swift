05f0b955dbc5f879da83b29173aabd76ff0643f8
YWSawyer <yuan_wei_1@163.com>
2017-08-30 18:10:56 +0800

    first import

---
+++ MVX-Swift/Controller/OtherController.swift
@@ -0,0 +1,38 @@
+//
+//  OtherController.swift
+//  MVX-Swift
+//
+//  Created by dahuoshi on 30/08/2017.
+//  Copyright © 2017 dahuoshi. All rights reserved.
+//
+
+import UIKit
+
+class OtherController: UIViewController {
+
+    @IBOutlet weak var infoBackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        var infoView: YWInfoView = Bundle.main.loadNibNamed("YWInfoView", owner: <#T##Any?#>, options: <#T##[AnyHashable : Any]?#>)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MVX-Swift/View/YWInfoView.swift
@@ -0,0 +1,27 @@
+//
+//  YWInfoView.swift
+//  MVX-Swift
+//
+//  Created by dahuoshi on 30/08/2017.
+//  Copyright © 2017 dahuoshi. All rights reserved.
+//
+
+import UIKit
+
+class YWInfoView: UIView {
+
+    @IBOutlet weak var headImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var productCountLbl: UILabel!
+    @IBOutlet weak var friendCountLbl: UILabel!
+    @IBOutlet weak var personalInfoLbl: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
