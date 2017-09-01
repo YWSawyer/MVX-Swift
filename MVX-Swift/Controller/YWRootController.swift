ebd8cca573f38efc372eaef2dd1cf4befe248f4b
YWSawyer <yuan_wei_1@163.com>
2017-09-01 18:02:41 +0800

    no message

--- MVX-Swift/Controller/YWRootController.swift
+++ MVX-Swift/Controller/YWRootController.swift
@@ -10,13 +10,27 @@ import UIKit
 
 class YWRootController: UITableViewController {
     
-    
+    lazy var datas: [YWRcModel] = {() -> [YWRcModel] in
+        var array: [YWRcModel] = [YWRcModel]()
+        let names = ["MVC","MVP","MVVM"]
+        let btn1Titles = ["查看他人信息","查看个人信息","查看个人信息"]
+        let btn2Titles = ["查看个人信息","",""]
+        for index in 0..<4 {
+            let model = YWRcModel()
+            model.name = names[index]
+            model.btn1 = btn1Titles[index]
+            model.btn2 = btn2Titles[index]
+            array.append(model)
+        }
+        return array
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+   
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,9 +44,29 @@ class YWRootController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return datas.count
+    }
+    
+    // MARK:- UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIndentifier = "rootCell"
+        let model = datas[indexPath.row]
+        var cell: YWRcCell? = tableView.dequeueReusableCell(withIdentifier: cellIndentifier) as? YWRcCell
+        if cell == nil {
+            cell = Bundle.main.loadNibNamed("YWRcCell", owner: nil, options: nil)?.last as? YWRcCell
+        }
+        cell?.nameLbl.text = model.name
+        cell?.btnOne.setTitle(model.btn1, for: .normal)
+        if indexPath.row == 2 {
+            cell?.btnTwo.setTitle(model.btn2, for: .normal)
+        }
+        return cell!
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
+    }
     /*
     // MARK: - Navigation
 
